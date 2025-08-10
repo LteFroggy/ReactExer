5aa919d34525e86f0584aed276fe6566decf281b
LteFroggy <zac0328@naver.com>
2025-08-10 17:48:56 +0900

    feat : App.tsx에서 라우팅 가능하도록 설정 추가

--- everyday/src/entry/App.tsx
+++ everyday/src/entry/App.tsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {MainPage} from "../pages/MainPage/MainPage.tsx";
+import {ReactPage} from "../pages/ReactPage/ReactPage.tsx";
+import {ChatPage} from "../pages/ChatPage/ChatPage.tsx";
+import {Header} from "../components/Header/Header.tsx";
+import {SetTokenPage} from "../pages/SetTokenPage/SetTokenPage.tsx";
+import {useState} from "react";
+import {SocketPage} from "../pages/SocketPage/SocketPage.tsx";
 
-function App() {
-  const [count, setCount] = useState(0)
+export const App = () => {
+  const [isConnected, setIsConnected] = useState(false);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+    <BrowserRouter>
+      <Header isConnected={isConnected} />
+      <Routes>
+        <Route path='/' element={<MainPage />} />
+        <Route path='/react' element={<ReactPage />} />
+        <Route path='/chat' element={<ChatPage />} />
+        <Route path='/settoken' element={<SetTokenPage />} />
+        <Route path='/socket' element={<SocketPage setIsConnected={setIsConnected} />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
