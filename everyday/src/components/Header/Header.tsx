aca92916c3bd8e8edd5db12d3d82bb9cd08c80ce
LteFroggy <zac0328@naver.com>
2025-08-13 12:22:05 +0900

    feat : 토스트 알림을 위한 부분 추가, 라우팅 수정 (#12)

--- everyday/src/components/Header/Header.tsx
+++ everyday/src/components/Header/Header.tsx
@@ -14,6 +14,8 @@ export const Header = ({ isConnected } : HeaderProps) => {
         <button onClick={() => navigate('/')}>메인 페이지로</button>
         <button onClick={() => navigate('/react')}>React 기본 페이지로</button>
         <button onClick={() => navigate('/chat/1')}>채팅 페이지로</button>
+        <button onClick={() => navigate('/socket')}>소켓 연결 페이지로</button>
+        <button onClick={() => navigate('/notification')}>알림 전송 페이지로</button>
         <button onClick={() => { window.location.href = "http://" + import.meta.env.VITE_ENDPOINT }}>로그인 페이지로</button>
       </div>
   )
